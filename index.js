c23f788c0f9fb9e697dbcc9018dbc39e746c9dc4
JGaratL <120282132+JGaratL@users.noreply.github.com>
2023-04-30 22:05:28 +0200

    Create index.js
    Hola Sergio, 
    te escribo aquí los comentarios.
    Lo he estructurado/dividido en secciones de la siguiente manera:
    1- Coger el nombre
    2- Coger el parámetro
    3- Identificar si el parámetro es un número. Es un número?
    4- Sí: Buscar pokemon con la id igual al parámetro.
    5- No:  Buscar pokemon con el name igual al parámetro.
    6- Crear response con datos del pokemon.
    
    Primero inportamos las librerías http y fs de lectura de ficheros.
    Creamos una función asíncrona que lea el fichero y lo pase a objetos js. Si ha ido bien convierte el texto en un array de objetos mediante la funcion JSON.parse.
    Después buscamos por nombre por los cuatro idiomas y comprobamos y lo mismo buscando por id.
    La función findPokemon nos ayuda a identificar si es un id o un nombre. 
    La función handleRequest la hacemos asíncrona por si llegase otro request no tenga q esperar, recoge el parámetro de la url que nos interesa, en esta caso solo hay uno. 
    El fetchPokemonData se asegura de leer el fichero una sola vez cuando arranca el servidor. Pasa el contenido del fichero a la variable data. Cuando el promise acaba bien levanta el servidor. Si no da error. 
    
    Mejoras: Se podría pasar el name del pokemon a minusculas y comprobar su coincidencia en minusculas ya q ahora daría problemas con mayusculas y minusculas.

---
+++ index.js
@@ -0,0 +1,74 @@
+const http = require('http'); 
+const fs = require('fs');
+
+
+const fetchPokemonData = async () => {
+   return new Promise((resolve, reject) => {
+       fs.readFile('./pokedex.json', 'utf-8', (err, data) => { 
+           if (err) {
+               reject(err); 
+           } else {
+               resolve(JSON.parse(data)); 
+           }
+       });
+   });
+};
+
+const findByName = (name) => {
+   return data.find(element => element.name.english == name
+       || element.name.japanese == name
+       || element.name.chinese == name
+       || element.name.french == name
+   );
+}
+const findById = (id) => {
+   return data.find(element => element.id == id);
+}
+
+const transformToPlainObject = (pokemon) => {
+   return {
+       "type": pokemon.type,
+       "HP": pokemon.base.HP,
+       "Attack": pokemon.base.Attack,
+       "Defense": pokemon.base.Defense,
+       "Sp. Attack": pokemon.base["Sp. Attack"],
+       "Sp. Defense": pokemon.base["Sp. Defense"],
+       "Speed": pokemon.base.Speed
+   };
+}
+
+const findPokemon = (param) => {
+   let paramIsName = isNaN(param); 
+   if (paramIsName)
+       return findByName(param);
+   else
+       return findById(param);
+}
+const handleRequest = async (req, res) => {
+   
+   const param = decodeURI(req.url.substring(1)); 
+   const pokemon = findPokemon(param);
+
+   if (responseData) {
+       res.writeHead(200, { 'Content-Type': 'application/json' }); 
+       res.end(JSON.stringify(transformToPlainObject(pokemon), null, 7));
+   } else {
+       res.writeHead(404, { 'Content-Type': 'text/plain' }); 
+       res.end('Pokemon no encontrado');
+   }
+};
+
+let data = null;
+
+fetchPokemonData() 
+   .then( (dataParam) => { 
+       data = dataParam; 
+       const server = http.createServer(handleRequest);
+
+       server.listen(3000, () => {
+           console.log('Servidor escuchando en el puerto 3000');
+       });
+   })
+   .catch((error) => { 
+       console.log("no se han podido leer los datos por lo que no tiene sentido seguir. Error code: "+ error.code);
+   });
